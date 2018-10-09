4fea54740fc8dd540b838ec36e6ef622cc32fd93
mateusfmachado <mateus1198@gmail.com>
2018-10-09 11:10:01 -0300

    adicinoado desafio2

---
+++ desafio-ecommerce/desafio2/precache-manifest.7459c4bcee333c51d8237a82ed646ecb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "12e8f092a03f7a493ff6",
+    "url": "/desafio-ecommerce/desafio2/static/js/runtime~main.12e8f092.js"
+  },
+  {
+    "revision": "fb6fa3b6dcac02e29ffb",
+    "url": "/desafio-ecommerce/desafio2/static/js/main.fb6fa3b6.chunk.js"
+  },
+  {
+    "revision": "872633813eb3847f201b",
+    "url": "/desafio-ecommerce/desafio2/static/js/1.87263381.chunk.js"
+  },
+  {
+    "revision": "fb6fa3b6dcac02e29ffb",
+    "url": "/desafio-ecommerce/desafio2/static/css/main.362cc1f8.chunk.css"
+  },
+  {
+    "revision": "beb8712482bd11a8b69783070e59a99f",
+    "url": "/desafio-ecommerce/desafio2/index.html"
+  }
+];
