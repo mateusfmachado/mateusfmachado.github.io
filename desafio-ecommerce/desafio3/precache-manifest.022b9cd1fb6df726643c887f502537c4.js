bc3f48a239905b8495e6ba373e9cb90ebee771ba
mateusfmachado <mateus1198@gmail.com>
2018-10-16 21:56:47 -0300

    adicionado desafio 3

---
+++ desafio-ecommerce/desafio3/precache-manifest.022b9cd1fb6df726643c887f502537c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c4ebcaba4f10386e6822",
+    "url": "/desafio-ecommerce/desafio3/static/js/runtime~main.c4ebcaba.js"
+  },
+  {
+    "revision": "aad17780eea9ecb101d0",
+    "url": "/desafio-ecommerce/desafio3/static/js/main.aad17780.chunk.js"
+  },
+  {
+    "revision": "4fd91c74e51506835aa9",
+    "url": "/desafio-ecommerce/desafio3/static/js/1.4fd91c74.chunk.js"
+  },
+  {
+    "revision": "aad17780eea9ecb101d0",
+    "url": "/desafio-ecommerce/desafio3/static/css/main.646ca446.chunk.css"
+  },
+  {
+    "revision": "7dd3bb162d744c9a5b38a6c11be8a99d",
+    "url": "/desafio-ecommerce/desafio3/index.html"
+  }
+];
