61ab4421d21f69f37c14b4ec6c9d40c350a8117b
mateusfmachado <mateus1198@gmail.com>
2018-10-23 08:26:52 -0300

    adicionado desafio 4

---
+++ desafio-ecommerce/desafio4/precache-manifest.0da5ff461b855b971d79adbb77645ed9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b03f7b3e8e29e8bba8fe",
+    "url": "/desafio-ecommerce/desafio4/static/js/runtime~main.b03f7b3e.js"
+  },
+  {
+    "revision": "10f51fbd5ae4580d59cc",
+    "url": "/desafio-ecommerce/desafio4/static/js/main.10f51fbd.chunk.js"
+  },
+  {
+    "revision": "4fd91c74e51506835aa9",
+    "url": "/desafio-ecommerce/desafio4/static/js/1.4fd91c74.chunk.js"
+  },
+  {
+    "revision": "10f51fbd5ae4580d59cc",
+    "url": "/desafio-ecommerce/desafio4/static/css/main.883ac707.chunk.css"
+  },
+  {
+    "revision": "241a2934bf6cf1e1a6672ed5365b0b6a",
+    "url": "/desafio-ecommerce/desafio4/index.html"
+  }
+];
---
+++ desafio-ecommerce/desafio4/precache-manifest.cd923ab6ae3c7bc775d67ebbe1e94fc8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b03f7b3e8e29e8bba8fe",
+    "url": "/desafio-ecommerce/desafio4/static/js/runtime~main.b03f7b3e.js"
+  },
+  {
+    "revision": "cdda8a80d408d15ab261",
+    "url": "/desafio-ecommerce/desafio4/static/js/main.cdda8a80.chunk.js"
+  },
+  {
+    "revision": "4fd91c74e51506835aa9",
+    "url": "/desafio-ecommerce/desafio4/static/js/1.4fd91c74.chunk.js"
+  },
+  {
+    "revision": "cdda8a80d408d15ab261",
+    "url": "/desafio-ecommerce/desafio4/static/css/main.883ac707.chunk.css"
+  },
+  {
+    "revision": "5c5af6ef7de146fd90b185e1ba00779e",
+    "url": "/desafio-ecommerce/desafio4/index.html"
+  }
+];
