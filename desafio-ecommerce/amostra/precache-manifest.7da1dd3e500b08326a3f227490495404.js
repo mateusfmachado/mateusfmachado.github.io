0e4ff34793f3a5832448126f3f9ef3fe4e15a4da
mateusfmachado <mateus1198@gmail.com>
2018-10-31 13:50:48 -0300

    adicinoado amostra

---
+++ desafio-ecommerce/amostra/precache-manifest.7da1dd3e500b08326a3f227490495404.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4c0843cbd60870d9469",
+    "url": "/desafio-ecommerce/amostra/static/js/runtime~main.d4c0843c.js"
+  },
+  {
+    "revision": "e05fec1fff31b5391638",
+    "url": "/desafio-ecommerce/amostra/static/js/main.e05fec1f.chunk.js"
+  },
+  {
+    "revision": "2345088808eca074ace8",
+    "url": "/desafio-ecommerce/amostra/static/js/1.23450888.chunk.js"
+  },
+  {
+    "revision": "e05fec1fff31b5391638",
+    "url": "/desafio-ecommerce/amostra/static/css/main.c3a74751.chunk.css"
+  },
+  {
+    "revision": "25173aa9bde728e22373a262be97d273",
+    "url": "/desafio-ecommerce/amostra/index.html"
+  }
+];
---
+++ desafio-ecommerce/amostra/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/desafio-ecommerce/amostra/precache-manifest.7da1dd3e500b08326a3f227490495404.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/desafio-ecommerce/amostra/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
