9e30de820da67ff6cf930785c16c36d8319fb46f
mateusfmachado <mateus1198@gmail.com>
2018-11-07 11:39:13 -0200

    adicionado desafio impala

---
+++ desafio-ecommerce/desafio6/precache-manifest.f1fc313aea149a6e956879513b472bb8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ffaf2ae55286b54584b",
+    "url": "/desafio-ecommerce/desafio6/static/js/runtime~main.9ffaf2ae.js"
+  },
+  {
+    "revision": "ac24ea7f8576add698fc",
+    "url": "/desafio-ecommerce/desafio6/static/js/main.ac24ea7f.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/desafio-ecommerce/desafio6/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "ac24ea7f8576add698fc",
+    "url": "/desafio-ecommerce/desafio6/static/css/main.350141a1.chunk.css"
+  },
+  {
+    "revision": "ca48602f02ca8b910c6cd8e7ec193655",
+    "url": "/desafio-ecommerce/desafio6/index.html"
+  }
+];
