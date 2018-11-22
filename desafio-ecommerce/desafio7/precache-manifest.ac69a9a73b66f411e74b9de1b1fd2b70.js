2465d4132f8e46f4098d2e1744dc7c0cc47ed5dd
mateusfmachado <mateus1198@gmail.com>
2018-11-22 16:46:35 -0200

    adicionado desafio 7

---
+++ desafio-ecommerce/desafio7/precache-manifest.ac69a9a73b66f411e74b9de1b1fd2b70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2564277b953a3046651c",
+    "url": "/desafio-ecommerce/desafio7/static/js/runtime~main.2564277b.js"
+  },
+  {
+    "revision": "28854c83466f2564ed02",
+    "url": "/desafio-ecommerce/desafio7/static/js/main.28854c83.chunk.js"
+  },
+  {
+    "revision": "3da870a3c1c4b41662f2",
+    "url": "/desafio-ecommerce/desafio7/static/js/1.3da870a3.chunk.js"
+  },
+  {
+    "revision": "28854c83466f2564ed02",
+    "url": "/desafio-ecommerce/desafio7/static/css/main.e36550a9.chunk.css"
+  },
+  {
+    "revision": "878e7c5ad9bcc4b51613dd2309751298",
+    "url": "/desafio-ecommerce/desafio7/index.html"
+  }
+];
