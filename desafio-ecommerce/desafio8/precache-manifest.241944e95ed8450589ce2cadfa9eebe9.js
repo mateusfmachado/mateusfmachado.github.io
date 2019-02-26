369380c0f4664f790f98896959cbe16027f9f35d
mateusfmachado <mateus1198@gmail.com>
2019-02-26 19:11:45 -0300

    adcionado desafio 8

---
+++ desafio-ecommerce/desafio8/precache-manifest.241944e95ed8450589ce2cadfa9eebe9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e22c66334051cb207062",
+    "url": "/static/js/main.e22c6633.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "e22c66334051cb207062",
+    "url": "/static/css/main.bc1f5c9f.chunk.css"
+  },
+  {
+    "revision": "81cf0ed8aafe734f284acec02c92ad42",
+    "url": "/index.html"
+  }
+];
