9921b098e790f5c46c07ec41c7e6e5c69de21ca5
mateusfmachado <mateus1198@gmail.com>
2019-02-28 16:48:56 -0300

    adicionado  correcoes mobile

--- desafio-ecommerce/desafio8/service-worker.js
+++ desafio-ecommerce/desafio8/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.241944e95ed8450589ce2cadfa9eebe9.js"
+  "/desafio-ecommerce/desafio8/precache-manifest.25138c889db8383a3f22408f9b6fd613.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/desafio-ecommerce/desafio8/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
