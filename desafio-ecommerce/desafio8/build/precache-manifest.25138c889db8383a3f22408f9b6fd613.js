80c31a35dc404b3910a7ef0e95da1c60aaafdb21
mateusfmachado <mateus1198@gmail.com>
2019-02-28 14:39:36 -0300

    Adicionado correcoes mobile

---
+++ desafio-ecommerce/desafio8/build/precache-manifest.25138c889db8383a3f22408f9b6fd613.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c4ad888fbec14dd41af7",
+    "url": "/desafio-ecommerce/desafio8/static/js/runtime~main.c4ad888f.js"
+  },
+  {
+    "revision": "aa39601ea6721bba651a",
+    "url": "/desafio-ecommerce/desafio8/static/js/main.aa39601e.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/desafio-ecommerce/desafio8/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "aa39601ea6721bba651a",
+    "url": "/desafio-ecommerce/desafio8/static/css/main.02a0cec1.chunk.css"
+  },
+  {
+    "revision": "81b551fdb2396b8bd655e7b808b4b8a9",
+    "url": "/desafio-ecommerce/desafio8/index.html"
+  }
+];
