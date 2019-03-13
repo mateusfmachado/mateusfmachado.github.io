081c937ef67d698db03aef275e1fbd0b0ff12110
mateusfmachado <mateus1198@gmail.com>
2019-03-13 09:32:32 -0300

    added desafio 9

---
+++ desafio-ecommerce/desafio9/precache-manifest.c53c9d619170a55129a3677ea90f3f5a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5c7b02fa4323602223e",
+    "url": "/desafio-ecommerce/desafio9/static/js/runtime~main.e5c7b02f.js"
+  },
+  {
+    "revision": "4e667e510d63517dfb73",
+    "url": "/desafio-ecommerce/desafio9/static/js/main.4e667e51.chunk.js"
+  },
+  {
+    "revision": "ac164eeb7fe39897100f",
+    "url": "/desafio-ecommerce/desafio9/static/js/2.ac164eeb.chunk.js"
+  },
+  {
+    "revision": "4e667e510d63517dfb73",
+    "url": "/desafio-ecommerce/desafio9/static/css/main.24db0d57.chunk.css"
+  },
+  {
+    "revision": "6e1099f1baa3c39f83bf7a3278366e79",
+    "url": "/desafio-ecommerce/desafio9/index.html"
+  }
+];
