214303b41624fd80adf4d541852295992e01d4e6
mateusfmachado <mateus1198@gmail.com>
2019-04-05 20:00:57 -0300

    adicionado desafio 12

---
+++ desafio-ecommerce/desafio12/precache-manifest.3f09429c07d9bfadac06510b6810f179.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "78d1ccd07ecdc763aa27",
+    "url": "/desafio-ecommerce/desafio12/static/js/runtime~main.46e810b9.js"
+  },
+  {
+    "revision": "3c0ed4a04190cb90e195",
+    "url": "/desafio-ecommerce/desafio12/static/js/main.0066aa92.chunk.js"
+  },
+  {
+    "revision": "b6aafa07ca16cd706e06",
+    "url": "/desafio-ecommerce/desafio12/static/js/2.2752fb64.chunk.js"
+  },
+  {
+    "revision": "3c0ed4a04190cb90e195",
+    "url": "/desafio-ecommerce/desafio12/static/css/main.36f7f296.chunk.css"
+  },
+  {
+    "revision": "3ffc98a6ae7ec65a961c46a1e2f41c20",
+    "url": "/desafio-ecommerce/desafio12/index.html"
+  }
+];
