fcc4c45fac73fdc7cf9472621267515a8bcbdc39
mateusfmachado <mateus1198@gmail.com>
2019-04-15 12:07:14 -0300

    adicionado desafio 13

---
+++ desafio-ecommerce/desafio13/precache-manifest.39f7ca657239d8d731f1d77800d98771.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "a0a04d67f58ba5cffcb3",
+    "url": "/static/js/main.9cb174ee.chunk.js"
+  },
+  {
+    "revision": "b6aafa07ca16cd706e06",
+    "url": "/static/js/2.2752fb64.chunk.js"
+  },
+  {
+    "revision": "a0a04d67f58ba5cffcb3",
+    "url": "/static/css/main.f24e00e5.chunk.css"
+  },
+  {
+    "revision": "801ae06dcf13c26239ffed1513228d24",
+    "url": "/index.html"
+  }
+];
