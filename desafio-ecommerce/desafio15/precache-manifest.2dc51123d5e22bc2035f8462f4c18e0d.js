34a49e1b97e5df9b2842400f04b62aa30373599d
mateusfmachado <mateus1198@gmail.com>
2019-04-27 13:26:15 -0300

    adicionado desafio 15

---
+++ desafio-ecommerce/desafio15/precache-manifest.2dc51123d5e22bc2035f8462f4c18e0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "41702136742572f759f8",
+    "url": "/desafio-ecommerce/desafio15/static/js/runtime~main.4fc69258.js"
+  },
+  {
+    "revision": "383e6d87acb5ee7aeaa3",
+    "url": "/desafio-ecommerce/desafio15/static/js/main.0de6939d.chunk.js"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/desafio-ecommerce/desafio15/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "383e6d87acb5ee7aeaa3",
+    "url": "/desafio-ecommerce/desafio15/static/css/main.eba2219a.chunk.css"
+  },
+  {
+    "revision": "51cf9386dfb1d582bcdc9ad0b8f9aa2c",
+    "url": "/desafio-ecommerce/desafio15/index.html"
+  }
+];
