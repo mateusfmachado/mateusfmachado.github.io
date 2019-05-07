25502b529727421b51d4b34273817a7a1c3294a2
mateusfmachado <mateus1198@gmail.com>
2019-05-07 13:03:32 -0300

    adicionado bela virtuosa

---
+++ desafio-ecommerce/desafio16/precache-manifest.2f7b7bce6381236dd33468f0b83db104.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "114a843a45714c2340540ae2d2842bb1",
+    "url": "/desafio-ecommerce/desafio16/index.html"
+  },
+  {
+    "revision": "f134a5e9d007cd4e3526",
+    "url": "/desafio-ecommerce/desafio16/static/css/main.1271979b.chunk.css"
+  },
+  {
+    "revision": "b6aafa07ca16cd706e06",
+    "url": "/desafio-ecommerce/desafio16/static/js/2.2752fb64.chunk.js"
+  },
+  {
+    "revision": "f134a5e9d007cd4e3526",
+    "url": "/desafio-ecommerce/desafio16/static/js/main.ab054c86.chunk.js"
+  },
+  {
+    "revision": "1cb4889a99502a9b674a",
+    "url": "/desafio-ecommerce/desafio16/static/js/runtime~main.4062cd63.js"
+  }
+]);
