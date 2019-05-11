36572b8c234bad2e62d26f73970a938b475ed6d1
mateusfmachado <mateus1198@gmail.com>
2019-05-10 22:54:00 -0300

    desafio 17

---
+++ desafio-ecommerce/desafio17/precache-manifest.3e48783e3ea0eca2bd8043aea3934faa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f60842391cb97d566b349bf65a8b3049",
+    "url": "/desafio-ecommerce/desafio17/index.html"
+  },
+  {
+    "revision": "cdcd233dfe23da967dc9",
+    "url": "/desafio-ecommerce/desafio17/static/css/main.58078882.chunk.css"
+  },
+  {
+    "revision": "ad30c0be719298edcd8b",
+    "url": "/desafio-ecommerce/desafio17/static/js/2.a022f2a8.chunk.js"
+  },
+  {
+    "revision": "cdcd233dfe23da967dc9",
+    "url": "/desafio-ecommerce/desafio17/static/js/main.7ff35181.chunk.js"
+  },
+  {
+    "revision": "c34babfa20093c50320d",
+    "url": "/desafio-ecommerce/desafio17/static/js/runtime~main.b3607f36.js"
+  }
+]);
---
+++ desafio-ecommerce/desafio17/precache-manifest.53a2c4a46382f885c62110f5653fe86e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bd49c56c4d9ff6c0fe863613192aec9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de63d37a2c859cbbaf6c",
+    "url": "/static/css/main.58078882.chunk.css"
+  },
+  {
+    "revision": "ad30c0be719298edcd8b",
+    "url": "/static/js/2.a022f2a8.chunk.js"
+  },
+  {
+    "revision": "de63d37a2c859cbbaf6c",
+    "url": "/static/js/main.285dcc7d.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);
