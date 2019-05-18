469a578989827ff27d84e38334dbaa59d0f06f80
mateusfmachado <mateus1198@gmail.com>
2019-05-18 12:46:24 -0300

    added 18

---
+++ desafio-ecommerce/desafio18/precache-manifest.f97874ec62634d61a53dd03f1911a1ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dfb8dc486cdf0559cf439c952e66500",
+    "url": "/desafio-ecommerce/desafio18/index.html"
+  },
+  {
+    "revision": "64803f67b796d2ea4d07",
+    "url": "/desafio-ecommerce/desafio18/static/css/main.e395696e.chunk.css"
+  },
+  {
+    "revision": "b6aafa07ca16cd706e06",
+    "url": "/desafio-ecommerce/desafio18/static/js/2.2752fb64.chunk.js"
+  },
+  {
+    "revision": "64803f67b796d2ea4d07",
+    "url": "/desafio-ecommerce/desafio18/static/js/main.635ec850.chunk.js"
+  },
+  {
+    "revision": "c1faf2b3ddb3ab3e0029",
+    "url": "/desafio-ecommerce/desafio18/static/js/runtime~main.a18b67bd.js"
+  }
+]);
